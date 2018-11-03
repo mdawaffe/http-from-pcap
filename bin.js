8a8942d55baeab4971d08fe7592e26dd5ae5a2dd
Michael D Adams <michael.d.adams@gmail.com>
2018-11-02 22:59:10 -0700

    Accept interface from argv. Better filter: only ipv4

--- bin.js
+++ bin.js
@@ -1,8 +1,25 @@
 #!/usr/bin/env node
+
+const fs = require( 'fs' )
+const path = require( 'path' )
+
 const pcap = require( 'pcap' )
 const HTTPFromPCAP = require( '.' )
 
-const pcapSession = pcap.createSession( 'lo0', 'tcp port 80' )
+const deviceOrPath = process.argv[2] || false
+
+const filter = 'ip proto \\tcp and port 80'
+
+let pcapSession
+if ( pcap.findalldevs().map( device => device.name ).includes( deviceOrPath ) ) {
+	pcapSession = pcap.createSession( deviceOrPath, filter )
+} else if ( fs.existsSync( deviceOrPath ) ) {
+	pcapSession = pcap.createOfflineSession( deviceOrPath, filter )
+} else {
+	const bin = path.basename( process.argv[1] )
+	console.error( `${bin} DEVICE\n${bin} PATH_TO_PCAP_DUMP_FILE` )
+	process.exit( 1 )
+}
 
 const httpFromPCAP = new HTTPFromPCAP( pcapSession )
 
